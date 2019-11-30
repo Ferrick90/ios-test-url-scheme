a49b76a0c5ce08aa0aba570a3844d08c34ac0d65
Sea Son Chan <sea.son.chan@comerge.net>
2019-11-30 11:55:08 +0800

    Initial Commit

---
+++ testinglink/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testinglink
+//
+//  Created by Ferrick Chan on 30/11/2019.
+//  Copyright © 2019 Ferrick Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
